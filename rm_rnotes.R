f234787281018ecbe2c730d74464c801de4f1326
androideruben <androide.ruben@gmail.com>
2018-05-11 09:00:30 -0400

    Update rm_rnotes.R

--- rm_rnotes.R
+++ rm_rnotes.R
@@ -171,4 +171,12 @@ XDF<-as.data.frame((rm_adbx))  #make data as data frame
 myvars<-names(XDF)[c(2,9,16,41,45,109)]
 X <- XDF[myvars]
 
+###Data frame:
+Died.At <- c(22,40,72,41)
+Writer.At <- c(16, 18, 36, 36)
+First.Name <- c("John", "Edgar", "Walt", "Jane")
+Second.Name <- c("Doe", "Poe", "Whitman", "Austen")
+Sex <- c("MALE", "MALE", "MALE", "FEMALE")
+Date.Of.Death <- c("2015-05-10", "1849-10-07", "1892-03-26","1817-07-18")
+writers_df <- data.frame(Died.At, Writer.At, First.Name, Second.Name, Sex, Date.Of.Death)
 
