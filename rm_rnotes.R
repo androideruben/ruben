f5ea436e7416ad4858a7f39a4116fc6c3a6c2533
androideruben <androide.ruben@gmail.com>
2018-05-15 11:39:11 -0400

    Update rm_rnotes.R

--- rm_rnotes.R
+++ rm_rnotes.R
@@ -39,6 +39,20 @@ sex <- function(data) {
 nhanes05$RIDRETH1 <-  race(nhanes05)
 nhanes05$RIAGENDR <-  sex(nhanes05)
 
+#proc freq; tables _all_:
+library(psych)
+psych::describe(rr[,sapply(rr, class) %in% c("numeric", "integer") ])
+
+# 2-Way Frequency Table
+attach(rr)
+mytable <- table(SMQ040,RIAGENDR) # A will be rows, B will be columns
+mytable # print table 
+margin.table(mytable, 1) # A frequencies (summed over B)
+margin.table(mytable, 2) # B frequencies (summed over A)
+
+prop.table(mytable) # cell percentages
+prop.table(mytable, 1) # row percentages
+prop.table(mytable, 2) # column percentages
 
 ##############################################
 
