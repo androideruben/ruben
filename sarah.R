2b2b65500b06fde8300af2f085fe89aef7612280
androideruben <androide.ruben@gmail.com>
2018-06-18 13:24:57 -0400

    Add files via upload

--- sarah.R
+++ sarah.R
@@ -9,6 +9,8 @@
 setwd("H:/Ruben.MontesdeOca/Documents")
 getwd()
 
+##https://tutorials.iq.harvard.edu/R/Rgraphics/Rgraphics.html
+
 library(Hmisc)
 mydata <- sasxport.get("H:/Ruben.MontesdeOca/Documents/sarah.xpt")
 summary(mydata)
@@ -17,3 +19,9 @@ head(mydata,10)
 ggplot(data=mydata, mapping=aes(x = visit, y = y)) + geom_point()
 
 ggplot(data=mydata, mapping=aes(x = visit, y = y, group=visit)) + geom_boxplot()
+
+ggplot(data=mydata, aes(x = visit), binwidth=1) +   geom_histogram()
+
+mydata2 <- subset(mydata, id='GCB 001') 
+ggplot(data=mydata2, mapping=aes(x = visit, y = y, group=visit)) + geom_boxplot()
+summary(mydata2)
