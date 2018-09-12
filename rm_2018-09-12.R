899cb510608652cb2e42c8d9f54a77a5ea48f1b7
androideruben <androide.ruben@gmail.com>
2018-09-12 13:37:52 -0400

    Add files via upload

--- rm_2018-09-12.R
+++ rm_2018-09-12.R
@@ -7,8 +7,8 @@
 ###########################################################################
 
 #0. working directory pointing to data sets
-#setwd("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-Misc/data/")
-setwd("rm_EXPLORE/0-Misc/data/")
+setwd("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-Misc/data/")
+#setwd("rm_EXPLORE/0-Misc/data/")
 getwd()
 
 #load libraries:
@@ -22,28 +22,45 @@ library(psych)
 patientID <- c('CK','CP','CP','FC','DS','JW','GS','HH','SS','BL','JI','RW','JW','SB','AB','FR','ES','MC','KD','BH','NS','EC')	
 Systolic 	<- c(120,120,165,125,110,134,122,122,96, 140,110,133,130,118,122,100,120,119,108,120,122,112)
 Diastolic <- c(50,75,11,76,50,80,70,82,60,90,40,60,80,76,78,70,70,66,54,65,78,62)
-BPressure <- data.frame(patientID, Systolic, Diastolic)
+treatment <- c('Y','Y','Y','N','N','N','Y','Y','N','N','Y','N','N','Y','Y','N','Y','Y','Y','N','N','Y')	
+
+BPressure <- data.frame(patientID, Systolic, Diastolic, treatment)
 	
+str(BPressure)
 summary(BPressure)
 describe(BPressure)	#mad: median absolute deviation (from the median)
+levels(BPressure$patientID)
+nlevels(BPressure$patientID)
+my_order <- sort(BPressure$patientID)
+my_data <- BPressure[order(BPressure$Systolic), ]
+my_data <- BPressure[order(-BPressure$Systolic), ]
 
 #output to a pdf:
 pdf("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-MISC/code/results/SystolicDiastolic.pdf")
 	plot(Systolic, Diastolic)
 	abline(lm(Systolic~Diastolic))
-	title("Systolic vs. Diastolic and Regression of Systolic on Diastolic")
+	#title("Systolic vs. Diastolic and Regression of Systolic on Diastolic")
 dev.off()
 
-#or png:
+#output to png:
 dev.copy(png,"//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-MISC/code/results/SystolicDiastolic.png")
 	plot(Systolic, Diastolic)
 	abline(lm(Systolic~Diastolic))
+		my_data
 dev.off()
 
-dev.copy(pdf,"//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-MISC/code/results/SystolicDiastolic.pdf")
-	plot(Systolic, Diastolic)
-	abline(lm(Systolic~Diastolic))
-dev.off()
+#output to txt:
+sink("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-MISC/code/results/SystolicDiastolic.txt")
+	my_data
+	#title("Systolic vs. Diastolic and Regression of Systolic on Diastolic")
+sink()
+
+#output to LateX:
+#https://cran.r-project.org/web/packages/tables/vignettes/tables.pdf
+library(tables)
+Hmisc::latex(
+tabular( (Species + 1) ~ (n=1) + Format(digits=2)*(Sepal.Length + Sepal.Width)*(mean), data=iris )
+)
 
 #data fitness:
 age 		<- c(44,44,38,40,44,44,45,54,51,48,57,52,51,51,49,52,40,42,47,43,38,45,47,49,51,49,54,50,54,57,48)
@@ -203,13 +220,24 @@ plot(x,y,type="l")
 curve(sin, to = 2*pi) 
 #curve(x, to = 2*pi) 
 
+#works with matrices:
+m <- matrix(data=cbind(rnorm(30, 0), rnorm(30, 2), rnorm(30, 5)), nrow=30, ncol=3)
+m
+str(m)
+plot(m[,1], m[,2])
+colnames(m) <- c("m0", "m2", "m5")
+m
+mean(m[,1])
+mean(m[,"m1"])
+mean(m[,3])
+
 #create new data using a function RxR->R
 col <- function(x,y) {airpass[,x]+y}
 col(1, 100)
 col(2, 2)
 
 airpass2 <- data.frame(col(1,100), col(2,1))
-names(airpass2) <- c("pass", "year")
+names(airpass2) <- c("pass", "year")  #rename variables
 str(airpass2)
 
 #7. compare data sets:
@@ -218,8 +246,7 @@ compare(airpass, airpass2, allowAll=T, by="pass")
 summary(compare(airpass, airpass2, allowAll=T))
 
 airpass
-summary(rr)
-curve(pasajeros~year, data=airpass)
+#curve(pass~year, data=airpass)
 
 sessionInfo()
 
@@ -266,105 +293,61 @@ fivenum(p29)
 
 
 ##more of importing data
-csvACS <-  read.csv("acs/ss05hak.csv", header=T, sep=",")
-head(csvACS)
-names(csvACS)
-typeof(csvACS)
+#csvACS <-  read.csv("acs/ss05hak.csv", header=T, sep=",")
+#head(csvACS)
+#names(csvACS)
+#typeof(csvACS)
 
 library(sas7bdat)
-sasACS <- read.sas7bdat("acs/psam_h02.sas7bdat",stringsAsFactors=FALSE) #not very good, read_sas is better
-head(sasACS)
-names(sasACS)
-str(csvACS)
-typeof(sasACS)
+#sasACS <- read.sas7bdat("acs/psam_h02.sas7bdat",stringsAsFactors=FALSE) #not very good, read_sas is better
+#head(sasACS)
+#names(sasACS)
+#str(csvACS)
+#typeof(sasACS)
 
 library(haven)
-
 sasACS <- read_sas("acs/psam_h02.sas7bdat", cols_only = c("SERIALNO",  "DIVISION", "PUMA",  "REGION", "ST", "ADJUST", "WGTP"))
 #zap_formats(sasACS)
-head(sasACS)
-names(sasACS)
-str(sasACS)
-typeof(sasACS)
+#head(sasACS)
+#names(sasACS)
+#str(sasACS)
+#typeof(sasACS)
 
 
-PATH_sas <- 'https://stats.idre.ucla.edu/wp-content/uploads/2016/02/binary.sas7bdat'
-df <- read_sas(PATH_sas)
-head(df)
+#PATH_sas <- 'https://stats.idre.ucla.edu/wp-content/uploads/2016/02/binary.sas7bdat'
+#df <- read_sas(PATH_sas)
+#head(df)
 
-install.packages("RCurl")
-library(RCurl)
-url <- download.file(url='https://www2.census.gov/programs-surveys/acs/data/pums/2005/pchak.zip', destfile='localfile.zip', method='curl')
+#install.packages("RCurl")
+#library(RCurl)
+#url <- download.file(url='https://www2.census.gov/programs-surveys/acs/data/pums/2005/pchak.zip', destfile='localfile.zip', method='curl')
+#df <- read_sas(path_sas)
 
+#library(arsenal)
+#compare(csvACS, sasACS, allowAll=T)
+#summary(compare(csvACS, sasACS, allowAll=T))
 
-df <- read_sas(path_sas)
-
-
-library(arsenal)
-compare(csvACS, sasACS, allowAll=T)
-summary(compare(csvACS, sasACS, allowAll=T))
-
-
-keep <- c("SERIALNO",  "DIVISION", "PUMA",  "REGION", "ST", "ADJUST", "WGTP")
+#keep <- c("SERIALNO",  "DIVISION", "PUMA",  "REGION", "ST", "ADJUST", "WGTP")
 #sas <- sasACS[keep]
-csv <- csvACS[keep]
+#csv <- csvACS[keep]
 
-summary(sas)
-summary(csv)
+#summary(sas)
+#summary(csv)
 
 library(xlsx)
 write.xlsx(csvACS, "acs/xlsACS.xlsx")
 
 library(readxl)
-xlsACS2 <- read_excel("acs/xlsACS.xlsx", col_names=T, sheet="Sheet1", range="B1:F15")
-xlsACS2
-names(xlsACS2)
-str(xlsACS2)
-typeof(xlsACS2)
+#xlsACS2 <- read_excel("acs/xlsACS.xlsx", col_names=T, sheet="Sheet1", range="B1:F15")
+#xlsACS2
+#names(xlsACS2)
+#str(xlsACS2)
+#typeof(xlsACS2)
 
-compare(sas, csv, allowAll=T, by= "SERIALNO")
-str(sas)
-str(csv)
-
-summary(compare(sas, csv, allowAll=T))
-
-#https://stackoverflow.com/questions/3171426/compare-two-data-frames-to-find-the-rows-in-data-frame-1-that-are-not-present-in
-library(sqldf)
-a1NotIna2 <- sqldf('SELECT * FROM sas EXCEPT SELECT * FROM csv')
-a1Ina2 <- sqldf('SELECT * FROM sasACS INTERSECT SELECT * FROM csvACS')
-
-library(dplyr) 
-anti_join(sas,csv)
-semi_join(sas,csv)
-
-attributes(sas$SERIALNO)
-attributes(csv$SERIALNO)
-
-summary(sas$SERIALNO)
-summary(csv$SERIALNO)
-
-sum(sasACS$SERIALNO)
-sum(csvACS$SERIALNO)
-
-rr <- as.numeric(as.character(sasACS$SERIALNO))
-rr
-sum(rr)
-attributes(rr)
-view(rr)
-a1NotIna2 <- sqldf('SELECT * FROM rr INTERSECT SELECT * FROM csvACS')
-
-
-
-str(sasACS$SERIALNO)
-str(csvACS$SERIALNO)
-setdiff(sasACS,csvACS)
-
-
-sasACS <- sasACS
-
-table1<-data.frame(id=c(1:5), animal=c("cat", "dog", "parakeet", "lion", "duck"))
-table2<-table1[c(1,3,5),]
-anti_join(table1, table2, by="id")
+#compare(sas, csv, allowAll=T, by= "SERIALNO")
+#str(sas)
+#str(csv)
+#summary(compare(sas, csv, allowAll=T))
 
 ##########################################################################################
 # End of rm_2018-09-12.R
