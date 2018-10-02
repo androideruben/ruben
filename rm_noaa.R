48dfccb8207d82d1ec2ef143b1421a9946a072b6
androideruben <androide.ruben@gmail.com>
2018-10-02 14:11:19 -0400

    Add files via upload

--- rm_noaa.R
+++ rm_noaa.R
@@ -10,33 +10,54 @@ setwd("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/
 #setwd("rm_EXPLORE/0-Misc/data/")
 getwd()
 
-#dev.copy(pdf,"//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-Misc/data/rm_noaaB.pdf")
-sink("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-Misc/data/rm_noaaB.txt")
-
-
 library(Hmisc)
 library(psych)
+library(xlsx)
+library(summarytools)
+library(descr)
 
 #1. read data set ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/legacy/
-StormData <-  read.csv("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-Misc/data/stormdata_2013.csv", header=T, sep=",")
-summary(StormData$BEGIN_DATE_TIME)
-head(StormData$BEGIN_DATE_TIME)
-nrow(StormData); ncol(StormData)
-length(StormData)
-str(StormData) #structure
-names(StormData)
-dim(StormData)
+##read document: http://www.nws.noaa.gov/directives/sym/pd01016005curr.pdf
 
-#2. work with dates:
-summary(StormData$BEGIN_DATE_TIME)  #date time
-my_date <- as.Date(StormData$BEGIN_DATE_TIME, format='%m/%d/%Y')
-summary(my_date) #can now calculate min, max...
-summary(StormData$MONTH_NAME)
+StormData0 <- read.csv("//fda.gov/WODC/CTP_Sandbox/OS/DPHS/StatisticsBranch/Team 2/Montes de Oca/rm_EXPLORE/0-Misc/data/stormdata_2013.csv", header=T, sep=",")
 
-#3. convert char  into numeric, add to data set and do plots:
+summary(StormData0)
+head(StormData0)
 
-##MONTH_NAME
-StormData$monthN <- ifelse(StormData$MONTH_NAME=="January", 1, 
+nrow(StormData0); ncol(StormData0); length(StormData0)
+
+str(StormData0) #structure
+sort(names(StormData0)) #alphabetic order of columns
+
+dim(StormData0)
+summary(StormData0$DAMAGE_PROPERTY)
+#complete.cases(StormData0$DAMAGE_PROPERTY)
+
+#2 make a usable subset
+##a. keep useful vars and obs (assume repeated events of rain are measures in different counties:
+StormData <- subset(StormData0[c("MONTH_NAME", "STATE", "FLOOD_CAUSE")], 
+																STATE %in% c("CALIFORNIA", "NEW YORK", "TEXAS") 
+																& !(FLOOD_CAUSE %in% c("Dam / Levee Break", "Planned Dam Release", "") ))
+View(StormData)
+summary(StormData) #disconcerting, structure keeps all the states but prints vales for selected ones
+
+#write.xlsx(StormData, file="StormData.xlsx", sheetName="one", append=FALSE)
+
+#3. convert char vars to numeric and create categories:
+
+##a. STATE
+StormData$stateN <- ifelse(StormData$STATE==    "CALIFORNIA",   1, 
+												ifelse(StormData$STATE=="NEW YORK",     2,
+												ifelse(StormData$STATE=="TEXAS",        3, NA)))
+
+#labels would make stateN a char, avoid that
+StormData$stateNL <- factor(StormData$stateN, levels=c(1,2,3), 
+															labels=c("California", "New York", "Texas"))	
+
+##b. MONTH_NAME
+unique(StormData$month) #only ten months
+
+StormData$monthN <- ifelse(StormData$MONTH_NAME==    "January",   1, 
 												ifelse(StormData$MONTH_NAME=="February",  2,
 												ifelse(StormData$MONTH_NAME=="March",     3,
 												ifelse(StormData$MONTH_NAME=="April",     4,
@@ -49,60 +70,57 @@ StormData$monthN <- ifelse(StormData$MONTH_NAME=="January", 1,
 												ifelse(StormData$MONTH_NAME=="November",  11,
 												ifelse(StormData$MONTH_NAME=="December",  12, 0))))))))))))
 
-unique(StormData$monthN); unique(StormData$STATE); unique(StormData$FLOOD_CAUSE) 
-keep=c("STATE", "MONTH_NAME", "monthN", "FLOOD_CAUSE")
-
-test <- StormData[keep]
-
-summary(subset(StormData, select=c(STATE, MONTH_NAME, monthN, FLOOD_CAUSE)))
-Hmisc::describe(test)
-psych::describe(test)
-psych::describeBy(test, test$STATE)
-psych::describeBy(test, test$STATE, mat=T)
-
-library(summarytools)
-dfSummary(test, style="grid", plain.ascii=T)
-
-##FLOOD_CAUSE
-StormData$floodN <- ifelse(StormData$FLOOD_CAUSE=="Heavy Rain",                       1, 
+StormData$monthNL <- factor(StormData$monthN, levels=c(1,2,3,4,5,6,7,8,9,10), 
+															labels=c("January", "February", "March", "April", "May", 
+																			 "June", "July", "August", "September", "October"))	
+##c. FLOOD_CAUSE
+StormData$floodN1 <- ifelse(StormData$FLOOD_CAUSE=="Heavy Rain",                      1, 
 												ifelse(StormData$FLOOD_CAUSE=="Ice Jam",                      2,
 												ifelse(StormData$FLOOD_CAUSE=="Heavy Rain / Snow Melt",       3,
 												ifelse(StormData$FLOOD_CAUSE=="Dam / Levee Break",            4,
 												ifelse(StormData$FLOOD_CAUSE=="Heavy Rain / Burn Area",       5,
 												ifelse(StormData$FLOOD_CAUSE=="Planned Dam Release",          6,
-												ifelse(StormData$FLOOD_CAUSE=="Heavy Rain / Tropical System", 7, 0)))))))
+												ifelse(StormData$FLOOD_CAUSE=="Heavy Rain / Tropical System", 7, NA)))))))
 
-keep=c("FLOOD_CAUSE", "floodN")
-test <- StormData[keep]
-head(test)
-Hmisc::describe(test)
-psych::describeBy(test, test$FLOOD_CAUSE)
-dfSummary(test, style="grid", plain.ascii=T)
+StormData$floodN1L <- factor(StormData$floodN, levels=c(1,2,3,4,5,6,7), labels=c("Heavy Rain", "Ice Jam",                    
+																																								"Heavy Rain / Snow Melt",     
+																																								"Dam / Levee Break",          
+																																								"Heavy Rain / Burn Area",     
+																																								"Planned Dam Release",        
+																																								"Heavy Rain / Tropical System"))	
+#c (cont'd). better option: group floodN:
+StormData$floodN2 <- ifelse(StormData$FLOOD_CAUSE %in% c("Heavy Rain",  
+																												"Heavy Rain / Snow Melt", 
+																												"Heavy Rain / Burn Area",
+																												"Heavy Rain / Tropical System"),           1, 
+	
+														ifelse(StormData$FLOOD_CAUSE=="Ice Jam",                               2,
+														ifelse(StormData$FLOOD_CAUSE %in% c("Dam / Levee Break",  
+																																"Heavy Rain / Tropical System"),   NA, 
+																																												           NA)))
 
-#put data together:
-keep=c("FLOOD_CAUSE", "floodN", "MONTH_NAME", "monthN", "STATE")
-StormData <- StormData[keep]
+StormData$floodN2L <- factor(StormData$floodN2, levels=c(1,2), labels=c("Rain related", "Ice"))	
+
+View(StormData)
+
+#4. Descriptives:
+psych::describeBy(StormData, StormData$MONTH_NAME)
+Hmisc::describe(StormData)
 dfSummary(StormData, style="grid", plain.ascii=T)
 
-#some descriptives:
-library(descr)
-CrossTable(StormData$floodN, StormData$monthN, expected=F, prop.chisq=F)
+CrossTable(StormData$monthN, StormData$stateN, expected=F, prop.chisq=F)
+hist(StormData$monthN,breaks=40,col="blue", xlab="floodN", xlim = c(-2, 12), main="floodN2", freq=F)
+boxplot(StormData$monthN~StormData$stateNL, xlab="xlab", ylab="ylab") 
 
-hist(StormData$floodN,breaks=40,col="blue", xlab="floodN", xlim = c(-2, 8), 
-					main="floodN", freq=F)
-hist(StormData$monthN,breaks=40,col="red", xlab="floodN", xlim = c(-2, 11), 
-					main="monthN", freq=F)
-
-boxplot(StormData$monthN~StormData$floodN, xlab="xlab", ylab="ylab") 
-boxplot(StormData$monthN~StormData$floodN, subset=StormData$floodN %in% c(1,2), xlab="xlab", ylab="ylab") 
 
 #chi square
 ##https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/chisq.test
-keep=c("floodN", "monthN")
+keep=c("floodN1", "stateN")
 
 StormData1 <- StormData[keep]
-xsq <- chisq.test(StormData1)
+xsq <- chisq.test(StormData1) #expected very small, approximations of p may not be right
 xsq
+View(StormData1)
 
 xsq$observed   # observed counts (same as M)
 xsq$expected   # expected counts under the null
@@ -115,27 +133,30 @@ head(xsqdata, 10)
 rcorr(as.matrix(StormData1))
 plot(StormData1[,1:2])
 
-#dev.off()
-sink()
+#5. chi square test of independence
+#(Ho: variables random and independent, small p-value rejects Ho)
 
-##########################################################################################
-# End of rm_noaa.R
-##########################################################################################
-
-
-curve(dchisq(x, df = 3), from = 0, to = 20, ylab = "y")
-ind <- c(4, 5, 10, 15)
-for (i in ind) curve(dchisq(x, df = i), 0, 20, add = TRUE)
-
-
-##columns of exercising, scores of heavy, never, ocassional, regular smoking:
-freq <- c(7, 87, 12, 9)
+##columns of exercising, and 4 scores of smoking: heavy, never, ocassional, regular
+freq <- c(7, 87, 12, 9) #7 are heavy smokers, 87 never, 12 ocassional, 9 regular
 none <- c(1, 18, 3, 1) 
 some <- c(3, 84, 4, 7)
 
-data1 <- data.frame(freq, none, some)
+###               Exercises No exercise Some Exercise |
+###Heavy smoking  7           1             3         |  11
+###No smoking    87          18            84         | 189
+###Occasional    12           3             4         |  19
+###Regular        9           1             7         |  17
+###---------------------------------------------------------
+###             115         23            98          | 236
 
-# products of margins give expected: 
+data1 <- data.frame(freq, none, some)
+names(data1) <- c("Exercises", "No exercise", "Some Exercise")
+data1
+
+xdata1 <- chisq.test(data1)
+xdata1 #X-squared = 5.4885, df = 6, p-value = 0.4828 (exercise is not associated to smoking.)
+
+##By hand:  products of margins give expected: 
 a=11*115/236
 b=189*115/236
 c=19*115/236
@@ -151,27 +172,25 @@ j=189*98/236
 k=19*98/236
 l=17*98/236
 
+a; b; c; d; e; f; g; h; i; j; k; l;
+
+
+
 x2= sum( (a-7)^2/ a, (b-87)^2 / b, (c-12)^2/ c, (d-9)^2 /d, 
 				 (e-1)^2 /e , (f-18)^2 /f , (g-3)^2 /g , (h-1)^2 /h ,
 				 (i-3)^2 /i , (j-84)^2 /j , (k-4)^2 /k , (l-7)^2 /l )
 x2
 
 #All cumulative probability functions in R compute left tail probabilities by default
-pchisq(x2, 6, lower.tail=F)
+pchisq(0x2, 6, lower.tail=F) #density
 1-pchisq(x2, 6, lower.tail=T)
-qchisq(0.95, 6)
+qchisq(0.95, 6) #quantile
 
-xdata1 <- chisq.test(data1)
-xdata1
+#plots
+x <- c(5.360169, 92.09746, 9.258475, 8.283898, 1.072034 ,18.41949 ,1.851695 ,1.65678 ,4.567797 ,78.48305 ,7.889831 ,7.059322)
+hist(x, prob=TRUE)	
+curve( dchisq(x, df=6), col='blue', add=F, xlim= c(-10, 100), ylab='y')
 
-curve(dchisq(x, df = 6), from = 0, to = 20, ylab = "y")
-
-
-library(HH)
- old.omd <- par(omd=c(0.05, 0.88, 0.05, 1.00))
- chisq.setup(df=6)
- chisq.curve(df=6, col='blue')
- chisq.observed(5.48, df=6)
- par(old.omd)
-
-
+##########################################################################################
+# End of rm_noaa.R
+##########################################################################################
